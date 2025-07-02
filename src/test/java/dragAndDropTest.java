2485c7d0ad709cd5192c2cca938eb457b1a6cbd4
Yana Yakovleva <yanna2013yanna@gmail.com>
2025-07-02 23:30:20 +0500

    Add dragAndDropTest

--- src/test/java/dragAndDropTest.java
+++ src/test/java/dragAndDropTest.java
@@ -1,2 +1,34 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
 public class dragAndDropTest {
+    @BeforeAll
+    static void setUp () {
+        Configuration.browserSize = "1920x1980";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com";
+        //Configuration.holdBrowserOpen = true; конфигурация для проверки теста
+    }
+
+    @Test
+    void solutionsEnterprize() {
+
+        //Открытие github
+        open("/git-guides");
+
+        //Открытие Solutions
+        $(withTagAndText("button", "Solutions")).hover();
+
+        //Клик по Enterprises
+        $(byText("Enterprises")).click();
+
+        //Проверка
+        $("#hero-section-brand-heading").shouldHave(exactText("The AI-powered\ndeveloper platform"));
+    }
 }
