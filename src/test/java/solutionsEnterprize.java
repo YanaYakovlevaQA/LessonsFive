9f737e46dfacd77fbcef910f6980895bc6af301e
Yana Yakovleva <yanna2013yanna@gmail.com>
2025-07-02 23:24:12 +0500

    Add test

---
+++ src/test/java/solutionsEnterprize.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class solutionsEnterprize {
+
+    @BeforeAll
+    static void setUp () {
+        Configuration.browserSize = "1920x1980";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.holdBrowserOpen = true; //конфигурация для проверки теста
+    }
+
+    @Test
+    void solutionsEnterprize() {
+
+        //Открытие github
+        open("/git-guides");
+
+        //Открытие Solutions
+        $(withTagAndText("button", "Solutions")).hover();
+        $(byText("Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(exactText("The AI-powered\ndeveloper platform"));
+}
+}
